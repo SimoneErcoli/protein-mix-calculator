0fc3358ba7241f567f352707c982b3457352e009
Simone Ercoli <simone.ercoli@drwolf.it>
2026-03-10 14:33:28 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/protein-mix-calculator',
+  assetPrefix: '/protein-mix-calculator',
   /* config options here */
 };
 
